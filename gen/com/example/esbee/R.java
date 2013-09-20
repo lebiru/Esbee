083d719658299a570eda574c687dd626f3b68071
zen6 <bilal.akabi@gmail.com>
2013-09-19 21:48:30 -0400

    added 'Sound Library' button

--- gen/com/example/esbee/R.java
+++ gen/com/example/esbee/R.java
@@ -24,11 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
+        public static final int action_settings=0x7f090006;
         public static final int button1=0x7f090002;
         public static final int button2=0x7f090001;
         public static final int button3=0x7f090004;
         public static final int button4=0x7f090003;
+        public static final int button5=0x7f090005;
         public static final int textView1=0x7f090000;
     }
     public static final class layout {

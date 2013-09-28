268009e5860228b720848efb883951ffeb61e4a9
zen6 <bilal.akabi@gmail.com>
2013-09-28 14:31:22 -0400

    linked four test sounds to four buttons

--- src/com/example/esbee/MainActivity.java
+++ src/com/example/esbee/MainActivity.java
@@ -11,21 +11,32 @@ import android.widget.Button;
 public class MainActivity extends Activity implements View.OnClickListener{
 
 	//Declare the MediaPlayer for the sound object mp1
-	MediaPlayer mp1;
+	MediaPlayer soundOne, soundTwo, soundThree, soundFour;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//Set the mp1 to the default sound
-		mp1 = MediaPlayer.create(this,  R.raw.one);
+		//Set the sounds to the default sound
+		soundOne = MediaPlayer.create(this,  R.raw.one);
+		soundTwo = MediaPlayer.create(this,  R.raw.two);
+		soundThree = MediaPlayer.create(this,  R.raw.three);
+		soundFour = MediaPlayer.create(this,  R.raw.four);
 		
 		//link the virtual button with the display button (?) not sure on this
 		final Button button1 = (Button) findViewById(R.id.button1);
+		final Button button2 = (Button) findViewById(R.id.button2);
+		final Button button3 = (Button) findViewById(R.id.button3);
+		final Button button4 = (Button) findViewById(R.id.button4);
+		
 		
 		//have the virtual button listen for clicks
 		button1.setOnClickListener(this);
+		button2.setOnClickListener(this);
+		button3.setOnClickListener(this);
+		button4.setOnClickListener(this);
 		
 	}
 
@@ -51,8 +62,24 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		{
 			//If button1 was clicked, play the sound
 			case R.id.button1:
-			mp1.start();
+			soundOne.start();
 			break;
+			
+			//If button2 was clicked, play the sound
+			case R.id.button2:
+			soundTwo.start();
+			break;
+			
+			//If button3 was clicked, play the sound
+			case R.id.button3:
+			soundThree.start();
+			break;
+			
+			//If button4 was clicked, play the sound
+			case R.id.button4:
+			soundFour.start();
+			break;
+			
 		}
 	}
 
